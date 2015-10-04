43f8b3aa11c5e639804f332a565b27c90d0d2294
Jett <lwanghe2@gmail.com>
2015-10-04 14:39:25 -0500

    init
    local files

---
+++ assets/temp/lang.js
@@ -0,0 +1,10 @@
+var home_en = ["home","events"];
+var home_cn = [“首页”,“活动”];
+
+function getName(){
+        return "yoo";
+}
+
+
+var cars = ["Saab", "Volvo", "BMW"];
+document.getElementById("demo").innerHTML = cars;
